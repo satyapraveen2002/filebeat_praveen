08b34b5d6edb92a61d1c879bd0ee03e696e4f87c
Praveen Kota <kotap@vmware.com>
2019-10-02 12:39:19 +0530

    Fourth Commit

--- plugins/log-intelligence-plugin/main.go
+++ plugins/log-intelligence-plugin/main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-  "os"
-  "fmt"
+	_ "github.com/raboof/beats-output-http"
 
-  _ "github.com/raboof/beats-output-http"
-
-	"github.com/elastic/beats/libbeat/beat"
-	"github.com/raboof/connbeat/beater"
 	"github.com/elastic/beats/libbeat/outputs"
 	"github.com/elastic/beats/libbeat/plugin"
-  "vmware-log-intelligence"
 )
 
 var Bundle = plugin.Bundle(
-	outputs.Plugin("VMware Log intelligence", vmware-log-intelligence.New),
+	outputs.Plugin("vmware-log-intelligence", vmware-log-intelligence.New),
 )
